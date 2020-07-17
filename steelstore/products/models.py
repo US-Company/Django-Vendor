0135b77498c9bdff8a0de49512555dca99104201
JhoselynQV <jhoselyn.quispe@upeu.edu.pe>
2020-07-17 16:19:42 -0500

    Jhoselyn

--- steelstore/products/models.py
+++ steelstore/products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+
+	category = models.CharField(max_length=225)
+	isbn = models.CharField(max_length=225)
+	create = models.DateTimeFiecategory
+	price = models.DecimalField(decimal_places=2, max_digits=6)
+	stock_level = models.IntegerField()
+
+	def __str__(self):
+		return self.category
